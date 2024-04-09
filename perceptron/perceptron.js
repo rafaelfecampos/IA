6fbc709efe0077eb96ba6a9f26089a243f525375
Rafael <2001campos.rafael@gmail.com>
2024-04-08 21:58:32 -0300

    aula 08/04

--- perceptron/perceptron.js
+++ perceptron/perceptron.js
@@ -1,10 +1,20 @@
 
 class Perceptron{
-    weights = [0,0];
+    weights = [];
+    lr = 0.1;
 
-    constructor(){
+    constructor(numberWeigths){
+        this.weights = new Array(numberWeigths)
         for(let i = 0;  i < this.weights.length; i++){
             this.weights[i] = random(-1,1);
+        }
+    }
+
+    train(input, target){
+        const guess = this.guess(input);
+        const error = target - guess;
+        for(let i = 0;  i < this.weights.length; i++){
+            this.weights[i] += error * input[i]*this.lr;
         }
     }
 
--- perceptron/sketch.js
+++ perceptron/sketch.js
@@ -1,13 +1,13 @@
 
-let perceptron
+let perceptron;
 
-let points = new Array(100);
+let points = new Array(500);
 
 function setup(){
     createCanvas(550,550);
+    perceptron = new Perceptron(2);
     for (let i = 0; i < points.length; i++) {
         points[i] = new Point(random(-1,1),random(-1,1));
-        // points[i].debug();
     }
 }
 
@@ -15,6 +15,41 @@ function draw(){
     background(255);
     points.forEach(point =>{
         point.show();
-    })
+    });
+    
+    noStroke();
 
+    points.forEach( pt =>{
+        const inputs = [pt.x, pt.y];
+        const target = pt.label;
+        const guess = perceptron.guess(inputs);
+        if (guess == target){
+            fill(0, 255, 0);
+        }else{
+            fill(255,0,0)
+        }
+
+        ellipse(pt.getPixelX(), pt.getPixelY(), 15,15)
+    });
+
+    drawLine();
+    trainSinglePoint();
+
+}
+
+function drawLine(){
+    stroke(0,0,255)
+    line(0,height,width,0);
+}
+
+let trainningIndex = 0;
+
+function trainSinglePoint(){
+    const pt = points[trainningIndex];
+    const inputs = [pt.x, pt.y];
+    perceptron.train(inputs, pt.label);
+    trainningIndex++;
+    if(trainningIndex == points.length){
+        trainningIndex = 0;
+    };
 }
